19b0a11a47e94b7d352f7a5a282e14cfa6121054
KJH-DESKTOP\akqjq <akqjqthr@gmail.com>
2022-02-14 07:37:27 +0900

    bema

---
+++ keyboards/boardsource/microdox/keymaps/bema/rules.mk
@@ -0,0 +1,13 @@
+MOUSEKEY_ENABLE     = yes     # Mouse keys
+EXTRAKEY_ENABLE = yes
+TAP_DANCE_ENABLE = yes
+RGBLIGHT_ENABLE     = no    # Enable WS2812 RGB underlight.
+RGB_MATRIX_ENABLE   = no
+# SLEEP_LED_ENABLE    = no
+# VIA_ENABLE          = yes         # Enable VIA
+# OLED_ENABLE = no
+# OLED_DRIVER = SSD1306
+# LTO_ENABLE          = yes
+
+# from default
+# SRC += muse.c
---
+++ keyboards/crkbd/keymaps/bema/rules.mk
@@ -0,0 +1,12 @@
+# MOUSEKEY_ENABLE     = yes     # Mouse keys
+TAP_DANCE_ENABLE = yes
+RGBLIGHT_ENABLE     = no    # Enable WS2812 RGB underlight.
+RGB_MATRIX_ENABLE   = yes
+# SLEEP_LED_ENABLE    = no
+VIA_ENABLE          = no         # Enable VIA
+OLED_ENABLE = no
+# OLED_DRIVER = SSD1306
+# LTO_ENABLE          = yes
+
+# from default
+# SRC += muse.c
